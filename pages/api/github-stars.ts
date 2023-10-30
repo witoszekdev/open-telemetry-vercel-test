00e59077cd8e5ae1319c54df669ab4f68afb4ab1
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-10-30 17:41:32 +0100

    Refactor structure

--- pages/api/github-stars.ts
+++ pages/api/github-stars.ts
@@ -1,10 +1,21 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { fetchGithubStars } from '../../shared/fetch-github-stars'
+import { NextApiRequest, NextApiResponse } from "next";
+
+export const runtime = "nodejs";
+
+async function fetchGithubStars() {
+  const res = await fetch("https://api.github.com/repos/vercel/next.js", {
+    next: {
+      revalidate: 0,
+    },
+  });
+  const data = await res.json();
+  return data.stargazers_count;
+}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
-  const stars = await fetchGithubStars()
-  res.status(200).json({ stars })
+  const stars = await fetchGithubStars();
+  res.status(200).json({ stars });
 }
