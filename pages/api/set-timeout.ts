ca25d4e3fb35a482a77b208192bd38f8544107d2
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-07 15:21:47 +0100

    Add request id

--- pages/api/set-timeout.ts
+++ pages/api/set-timeout.ts
@@ -11,20 +11,23 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const content = req.body;
+  const requestId =
+    (req.headers["x-vercel-proxy-signature-ts"] as string) ??
+    "<unknown-request-id>";
 
-  console.log("handling request", content, req.headers);
-  console.time();
+  console.log("handling request", content, requestId);
+  console.time(requestId);
   const handle = setTimeout(() => {
     const timeoutId = handle[Symbol.toPrimitive]();
-    console.log("hello from timeout", { timeoutId, content });
+    console.log("hello from timeout", { timeoutId, content, requestId });
     console.time(`timeout-${timeoutId}`);
     fetch("https://webhook.site/bc9aaa76-cf2b-42d0-b8ab-9954e22ddbea");
     console.timeEnd(`timeout-${timeoutId}`);
   }, 4_000);
-  console.timeLog();
+  console.timeLog(requestId, "before wait");
   await wait();
-  console.timeLog();
+  console.timeLog(requestId, "before response");
   res.status(200).json({ ok: true, content });
-  console.timeLog();
+  console.timeLog(requestId, "after response");
   // clearTimeout(id);
 }
