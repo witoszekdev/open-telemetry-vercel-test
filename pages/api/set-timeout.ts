f68b1390baaa7b0865d8012592b5f2002b9da87d
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-07 15:41:52 +0100

    Add manual span

--- pages/api/set-timeout.ts
+++ pages/api/set-timeout.ts
@@ -1,3 +1,4 @@
+import { trace } from "@opentelemetry/api";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export const runtime = "nodejs";
@@ -10,31 +11,53 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const content = req.body;
-  const requestId =
-    (req.headers["x-vercel-proxy-signature-ts"] as string) ??
-    "<unknown-request-id>";
+  return await trace
+    .getTracer("example-nextjs-app")
+    .startActiveSpan("set-timeout", async (span) => {
+      const content = req.body;
+      const requestId =
+        (req.headers["x-vercel-proxy-signature-ts"] as string) ??
+        "<unknown-request-id>";
 
-  console.log("handling request", content, requestId);
-  console.time(requestId);
-  const handle = setTimeout(async () => {
-    const timeoutId = handle[Symbol.toPrimitive]();
-    console.log("hello from timeout", { timeoutId, content, requestId });
-    console.time(`timeout-${timeoutId}`);
-    try {
-      await fetch("https://webhook.site/bc9aaa76-cf2b-42d0-b8ab-9954e22ddbea", {
-        body: JSON.stringify({ requestId, content, timeoutId }),
-        method: "POST",
-      });
-    } catch (e) {
-      console.error("error in timeout", e);
-    }
-    console.timeEnd(`timeout-${timeoutId}`);
-  }, 4_000);
-  console.timeLog(requestId, "before wait");
-  await wait();
-  console.timeLog(requestId, "before response");
-  res.status(200).json({ ok: true, content });
-  console.timeLog(requestId, "after response");
-  // clearTimeout(id);
+      span.setAttribute("content", content);
+      span.setAttribute("requestId", requestId);
+
+      console.log("handling request", content, requestId);
+      console.time(requestId);
+      const handle = setTimeout(async () => {
+        await trace
+          .getTracer("example-nextjs-app")
+          .startActiveSpan("timeout", async () => {
+            const timeoutId = handle[Symbol.toPrimitive]();
+            span.setAttribute("timeoutId", timeoutId);
+            span.setAttribute("content", content);
+            span.setAttribute("requestId", requestId);
+            console.log("hello from timeout", {
+              timeoutId,
+              content,
+              requestId,
+            });
+            console.time(`timeout-${timeoutId}`);
+            try {
+              await fetch(
+                "https://webhook.site/bc9aaa76-cf2b-42d0-b8ab-9954e22ddbea",
+                {
+                  body: JSON.stringify({ requestId, content, timeoutId }),
+                  method: "POST",
+                },
+              );
+            } catch (e) {
+              console.error("error in timeout", e);
+            }
+            console.timeEnd(`timeout-${timeoutId}`);
+          });
+      }, 4_000);
+      console.timeLog(requestId, "before wait");
+      await wait();
+      console.timeLog(requestId, "before response");
+      res.status(200).json({ ok: true, content });
+      console.timeLog(requestId, "after response");
+      span.end();
+      // clearTimeout(id);
+    });
 }
