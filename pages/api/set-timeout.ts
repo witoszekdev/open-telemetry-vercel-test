792fceaf07f6498bfb0c4a15fb1040cf1100cee2
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-07 15:07:03 +0100

    Add content

--- pages/api/set-timeout.ts
+++ pages/api/set-timeout.ts
@@ -10,17 +10,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  const content = req.body;
+
+  console.log("handling request", content);
   console.time();
-  const id = setTimeout(() => {
-    console.log("hello from timeout");
-    console.timeLog("request start");
+  const handle = setTimeout(() => {
+    const timeoutId = handle[Symbol.toPrimitive]();
+    console.log("hello from timeout", { timeoutId, content });
+    console.time(`timeout-${timeoutId}`);
     fetch("https://webhook.site/bc9aaa76-cf2b-42d0-b8ab-9954e22ddbea");
-    console.timeEnd();
+    console.timeEnd(`timeout-${timeoutId}`);
   }, 4_000);
   console.timeLog("before wait");
   await wait();
   console.timeLog("before response");
-  res.status(200).json({ ok: true });
+  res.status(200).json({ ok: true, content });
   console.timeLog("after response");
   // clearTimeout(id);
 }
