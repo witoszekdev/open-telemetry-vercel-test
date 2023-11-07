52aa1f8d2dd20b8b351d5992216a62ed8d749c92
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-07 14:28:18 +0100

    Add bigger timeout

--- pages/api/set-timeout.ts
+++ pages/api/set-timeout.ts
@@ -11,7 +11,7 @@ export default async function handler(
     console.log("hello from timeout");
     console.timeEnd("timer");
     fetch("http://google.com");
-  }, 1000);
+  }, 10_000);
   console.timeLog("timer", "before response");
   res.status(200).json({ ok: true });
   console.timeLog("timer", "after response");
