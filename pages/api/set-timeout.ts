038c7211370027d0bc35eab2c87f25e1857e520d
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-07 14:35:31 +0100

    Add request to webhook.site

--- pages/api/set-timeout.ts
+++ pages/api/set-timeout.ts
@@ -10,8 +10,8 @@ export default async function handler(
   const id = setTimeout(() => {
     console.log("hello from timeout");
     console.timeEnd("timer");
-    fetch("http://google.com");
-  }, 10_000);
+    fetch("https://webhook.site/bc9aaa76-cf2b-42d0-b8ab-9954e22ddbea");
+  }, 4_000);
   console.timeLog("timer", "before response");
   res.status(200).json({ ok: true });
   console.timeLog("timer", "after response");
