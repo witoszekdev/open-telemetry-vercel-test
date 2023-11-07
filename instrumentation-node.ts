97cb753175cc30b0b95c9b1d58d2c577aed2b391
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-07 14:18:22 +0100

    Add instrumentation http

--- instrumentation-node.ts
+++ instrumentation-node.ts
@@ -3,12 +3,16 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: "example-nextjs-app",
   }),
   spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+  instrumentations: [new HttpInstrumentation()],
 });
 
 sdk.start();
