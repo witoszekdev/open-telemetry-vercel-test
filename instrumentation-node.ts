e4c48ef5e8ff534c8a435947ace731f80bdff0b1
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-08 10:48:08 +0100

    Move initialize into a function

--- instrumentation-node.ts
+++ instrumentation-node.ts
@@ -6,15 +6,18 @@ import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 export const spanProcessor = new SimpleSpanProcessor(new OTLPTraceExporter());
 
-const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "example-nextjs-app",
-  }),
-  spanProcessor: spanProcessor,
-  instrumentations: [new HttpInstrumentation()],
-});
+export const initialize = () => {
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+  const sdk = new NodeSDK({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: "example-nextjs-app",
+    }),
+    spanProcessor: spanProcessor,
+    instrumentations: [new HttpInstrumentation()],
+  });
 
-sdk.start();
+  sdk.start();
+  return sdk;
+};
--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./instrumentation-node");
+    const { initialize } = await import("./instrumentation-node");
+    initialize();
   }
 }
