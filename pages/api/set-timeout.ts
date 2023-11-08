177de0cd29effa8c0a829a253f858c6b2e0182f1
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-08 16:44:34 +0100

    Test different span name

--- pages/api/set-timeout.ts
+++ pages/api/set-timeout.ts
@@ -12,29 +12,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const result = await trace
-    .getTracer("example-nextjs-app")
-    .startActiveSpan("set-timeout-route", async (span) => {
-      const content = req.body;
-      const requestId =
-        (req.headers["x-vercel-proxy-signature-ts"] as string) ??
-        "<unknown-request-id>";
+  const span = trace.getTracer("something-else").startSpan("set-timeout-route");
+  const content = req.body;
+  const requestId =
+    (req.headers["x-vercel-proxy-signature-ts"] as string) ??
+    "<unknown-request-id>";
 
-      span.setAttribute("content", content);
-      span.setAttribute("requestId", requestId);
+  span.setAttribute("content", content);
+  span.setAttribute("requestId", requestId);
 
-      console.log("handling request", content, requestId);
-      console.time(requestId);
-      console.timeLog(requestId, "before wait");
-      await wait();
-      console.timeLog(requestId, "before response");
-      span.end();
-      await spanProcessor.forceFlush();
-      res.status(200).json({ ok: true, content });
-      console.timeLog(requestId, "after response");
-      await wait(1000);
-      await spanProcessor.forceFlush();
-      console.timeLog(requestId, "wait 1000ms + flush");
-      // clearTimeout(id);
-    });
+  console.log("handling request", content, requestId);
+  console.time(requestId);
+  console.timeLog(requestId, "before wait");
+  await wait();
+  console.timeLog(requestId, "before response");
+  span.end();
+  await spanProcessor.forceFlush();
+  res.status(200).json({ ok: true, content });
+  console.timeLog(requestId, "after response");
 }
