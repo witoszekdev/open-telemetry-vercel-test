9c6130e8d05e879baf1206129041f6167b5fbad2
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-11-14 13:42:00 +0100

    Add debug span processor

--- my-instrumentation.ts
+++ my-instrumentation.ts
@@ -3,13 +3,11 @@ import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { spanProcessor } from "./utils";
 
 console.log("running instrumentation");
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-export const spanProcessor = new SimpleSpanProcessor(new OTLPTraceExporter());
 
 export const sdk = new NodeSDK({
   resource: new Resource({
--- utils.ts
+++ utils.ts
@@ -1,8 +1,50 @@
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import {
+  BatchSpanProcessor,
+  SpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
+import { Span, Context } from "@opentelemetry/api";
+import { ReadableSpan } from "@opentelemetry/sdk-trace-base";
+// import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
 
-export const spanProcessor = new SimpleSpanProcessor(
-  new OTLPTraceExporter({
-    url: "https://otel.plur.tech/v1/traces",
-  }),
-);
+// Debug span processor
+export class CustomBatchSpanProcessor extends BatchSpanProcessor {
+  onEnd(span: ReadableSpan): void {
+    if (span.instrumentationLibrary.name === "next.js") {
+      console.log("Dropping next.js spans");
+    } else {
+      super.onEnd(span);
+    }
+  }
+}
+
+export class CustomBatchSpanProcessorBis implements SpanProcessor {
+  public finishedSpans: ReadableSpan[] = [];
+
+  async forceFlush(): Promise<void> {
+    console.log("Force flush");
+    if (this.finishedSpans.length > 0) {
+      console.log(`Flushing ${this.finishedSpans.length} spans`);
+    }
+  }
+
+  onStart(_span: Span, _parentContext: Context): void {}
+
+  onEnd(span: ReadableSpan): void {
+    if (span.instrumentationLibrary.name === "next.js") {
+      console.log("Dropping next.js spans");
+    } else {
+      this.finishedSpans.push(span);
+    }
+  }
+
+  async shutdown(): Promise<void> {
+    console.log("Shutdown");
+  }
+}
+
+export const spanProcessor = new CustomBatchSpanProcessorBis();
+// export const spanProcessor = new SimpleSpanProcessor(
+//   new OTLPTraceExporter({
+//     url: "https://otel.plur.tech/v1/traces",
+//   }),
+// );
